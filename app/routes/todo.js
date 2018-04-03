f78a0b90ca0a7f661b8088da266f0894a6465117
felipekim9 <felipekim9@gmail.com>
2018-04-03 19:50:51 -0300

    create action create, install localstorage, install route-action

--- app/routes/todo.js
+++ app/routes/todo.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import {storageFor} from 'ember-local-storage';
 
 export default Route.extend({
+  todo: storageFor('todo'),
+  actions: {
+    createItem(e){
+      if (e.keyCode === 13) {
+        this.get('todo').addObject(e.target.value)
+      }
+    }
+  }
+
 });
