ac049e739b9f335303e06bffc75de2e306cdf773
felipekim9 <felipekim9@gmail.com>
2018-05-09 07:37:50 -0300

    Create todo counter.

--- app/components/info-label.js
+++ app/components/info-label.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
+  total: reads('model.length'),
+  totalMessage: computed('total', function() {
+    let total = this.get('total');
+
+    if (total > 1) {
+      return "Itens na lista";
+    } else {
+      return "Item na lista";
+    }
+  })
 });
